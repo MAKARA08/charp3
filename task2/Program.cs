dd4ce4a8aaf1a90464e21bcf257fac0a7ed951d1
Nastya <nastymakarivich@gmail.com>
2023-01-14 00:03:48 +0300

    Сделала ДЗ

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите координаты x, y, z точки А");
+int ax = Convert.ToInt32(Console.ReadLine());
+int ay = Convert.ToInt32(Console.ReadLine());
+int az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите координаты x, y, z точки B");
+int bx = Convert.ToInt32(Console.ReadLine());
+int by = Convert.ToInt32(Console.ReadLine());
+int bz = Convert.ToInt32(Console.ReadLine());
+
+int countx = (bx - ax) * (bx - ax);
+int county = (by - ay) * (by - ay);
+int countz = (bz - az) * (bz - az);
+
+double result = Math.Round(Math.Sqrt(countx + county + countz), 2);
+Console.WriteLine(result);
