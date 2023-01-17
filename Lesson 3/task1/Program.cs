2f3e6dd985dd52201de008bb7a1a73881ea1dc35
«nastya» <nastya@example.com>
2023-01-17 22:01:48 +0300

    дз

---
+++ Lesson 3/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿String number = Console.ReadLine();
+String[] array = new String[Convert.ToInt32(number.Length)];
+
+for (int i = 0; i < array.Length; i++) {
+    array[i] = number.Substring(i, 1);
+}
+
+for(int i = 0; i < array.Length; i++) {
+    if (i == 2) {
+        Console.WriteLine("да");
+        break;
+    } else if (array[i] != array[array.Length - 1 - i]) {
+        Console.WriteLine("нет");
+        break;
+    } 
+}
---
+++ Lesson 3/task3/Program.cs
@@ -0,0 +1,2 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++) {Console.WriteLine(i * i * i);}
